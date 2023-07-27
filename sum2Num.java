df10022874eb7fe39375a5e7d32d2533c1fd0507
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-07-27 14:36:54 +0530

    Q2
    sum of 2 num

---
+++ sum2Num.java
@@ -0,0 +1,13 @@
+public class sum2Num {
+
+    public static int sum(int a, int b){
+        int sumOf2 = a + b;
+        return sumOf2;
+    }
+    public static void main(String args[]){
+        int a = 5;
+        int b = 10;
+        int sumOf2 = sum(a, b);
+        System.out.println("a + b = " + sumOf2);
+    }
+}

56fb7db3da994e45e03c3c0188781a0fbd9a23af
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-07-28 18:59:09 +0530

    Q14

--- Q14.java
+++ Q14.java
@@ -1,3 +1,27 @@
+// Write a Java program to calculate the sum of two integers and return true if the sum is equal to a third integer.
+// Sample Output:
+
+// Input the first number : 5                                             
+// Input the second number: 10                                            
+// Input the third number : 15 
+
+import java.util.*;
+
 public class Q14 {
-    
+     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        find(a, b, c);
+     }    
+
+     public static void find (int a, int b , int c){
+        if((a+b)==c){
+        System.out.println("true");
+      }else{
+        System.out.println("false");
+      }
+        
+     }
 }

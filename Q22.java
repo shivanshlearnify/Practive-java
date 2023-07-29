3a2003866132124e87c1e6adb37a5e57abdb17ff
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-07-29 19:09:17 +0530

    till q23

--- Q22.java
+++ Q22.java
@@ -1,3 +1,20 @@
+// 31. Write a program that accepts three numbers from the user and prints "increasing" if the numbers are in increasing order, "decreasing" if the numbers are in decreasing order, and "Neither increasing or decreasing order" otherwise.
+
+import java.util.*;
 public class Q22 {
-    
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        find(a, b, c);
+
+    }
+    public static void find(int a, int b, int c){
+        if(c>b  && b>a){
+            System.out.println("incresaing order");
+        }else {
+            System.out.println("Decresaing order");
+        }
+    }
 }
---
+++ Q23.java
@@ -0,0 +1,12 @@
+// 14. Write a Java program to display the multiplication table of a given integer.
+
+
+
+public class Q23 {
+    public static void main(String args[]){
+        for(int i=1; i<=10; i++){
+            int multiplication = 2*i;
+            System.out.println(multiplication);
+        }
+    }
+}

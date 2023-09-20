65d5c5e2c3c1a3d82540a287a76d5e0ba4da14d5
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-09-20 20:39:04 +0530

    done

---
+++ Recursion/FactorialOfN.java
@@ -0,0 +1,12 @@
+public class FactorialOfN {
+    public static int fac(int n){
+        if(n==0){
+            return 1 ;
+        }
+        return n*fac(n-1);
+    }
+       public static void main(String args[]){
+        int n = 5;
+        System.out.println(fac(n));
+    }
+}

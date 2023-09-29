a4ac5aa0c1c84b0a64e44cc6654673ccab5a7ba8
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-09-29 15:53:35 +0530

    till today

--- Dynamic Programing/LCSubstring.java
+++ Dynamic Programing/LCSubstring.java
@@ -1,35 +1,34 @@
-public class LCS_withTabulation {
-    public static int lcs(String str1, String str2) {
+public class LCSubstring {
+
+    public static int lcs(String str1, String str2){
         int n = str1.length();
         int m = str2.length();
-        int dp[][] = new int[n + 1][m + 1];
+        int dp[][] = new int[n+1][m+1];
+        int ans = 0;
 
         for(int i=0; i<n+1; i++){
             for(int j=0; j<m+1; j++){
                 if(i==0 || j==0){ 
                 dp[i][j] = 0;
             }
-        }
+         }
         }
         for(int i=1; i<n+1; i++){
             for(int j=1; j<m+1; j++){
                if(str1.charAt(i-1) == str2.charAt(j-1)){
                 dp[i][j] = dp[i-1][j-1]+1;
+                ans = Math.max(ans, dp[i][j]);
                } else {
-                int ans1 = dp[i-1][j];
-                int ans2 = dp[i][j-1];
-                dp[i][j] = Math.max(ans1, ans2);
+                 dp[i][j] = 0;
                }
             }
         }
-        return dp[n][m];
-        
+      return ans;
+
     }
-
-    public static void main(String args[]) {
-        String str1 = "abcde";
-        String str2 = "ace";
-
+    public static void main(String args[]){
+        String str1 = "ABCDE";
+        String str2 = "ABCE";
         System.out.println(lcs(str1, str2));
     }
 }

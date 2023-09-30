8ac9f0df7555c3b5e7fb69e6a8a6e57f75a968c4
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-09-30 17:03:02 +0530

    Create CatalanNumber_With_memoization.java

---
+++ Dynamic Programing/CatalanNumber_With_memoization.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class CatalanNumber_With_memoization {
+    public static int find(int n, int dp[]){
+        int ans = 0;
+      if(n==0 || n==1){
+        return 1;
+      }
+      if(dp[n] != -1){
+        return dp[n];
+      }
+      for(int i=0; i<=n-1; i++){
+        ans += find(i, dp)*find(n-i-1, dp);
+      }
+      return dp[n] = ans;
+
+    }
+    public static void main(String args[]){
+        int n=20;
+        
+
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(find(n, dp));
+    }
+}

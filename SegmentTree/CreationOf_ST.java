bd497e5ad50b4d7af019e93f7ecccc9b1cbfb521
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-10-04 21:03:26 +0530

    Create CreationOf_ST.java

---
+++ SegmentTree/CreationOf_ST.java
@@ -0,0 +1,32 @@
+public class CreationOf_ST {
+    static int tree[];
+
+    public static void init(int n) {
+        tree = new int[4 * n];
+
+    }
+
+    public static int buildST(int arr[], int SegTreeidx, int start, int end) {
+        if (start == end) {
+            tree[SegTreeidx] = arr[start];
+            return arr[start];
+        }
+
+        int mid = (start + end) / 2;
+        buildST(arr, 2 * SegTreeidx + 1, start, mid); // left subtree - 2*i+1
+        buildST(arr, 2 * SegTreeidx + 2, mid + 1, end); // right subtree - 2*i+2
+        tree[SegTreeidx] = tree[2 * SegTreeidx + 1] + tree[2 * SegTreeidx + 2];
+        return tree[SegTreeidx];
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int n = arr.length;
+        init(n);
+        buildST(arr, 0, 0, n - 1);
+
+        for(int i=0; i<tree.length; i++){
+            System.out.print(tree[i]+" ");
+        }
+    }
+}
